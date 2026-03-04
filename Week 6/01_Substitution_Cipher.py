fac4d043ac5bb408acc7ea0b71343138bd4a37f1
Shiv Gobind Mourya <shivgobindmourya@gmail.com>
2026-03-04 22:38:07 +0530

    substitution cipher

---
+++ Week 6/01_Substitution_Cipher.py
@@ -0,0 +1,23 @@
+import string
+
+dict = {}
+data = ""
+file = open("Week 6/demo01.txt", "w")
+for i in range (len(string.ascii_letters)):
+    dict[string.ascii_letters[i]] = string.ascii_letters[i-1]
+
+print(dict)
+
+with open("Week 6/demo.txt") as f:
+    while True:
+        c=f.read(1)
+        if not c:
+            print("End of file")
+            break
+        if c in dict:
+            data = dict[c]
+        else:
+            data = c
+        file.write(data)
+        print(data)
+file.close()
